59612a5dde78425a260f67d6fb71df130282d61e
surajshah20 <suraj27227@gmail.com>
2026-06-17 22:37:56 +0545

    Fix Google OAuth

--- server/config/passport.js
+++ server/config/passport.js
@@ -7,10 +7,10 @@ const db = require("../db");
 
 // ✅ Bulletproof Callback URL routing
 const isProd = process.env.NODE_ENV === "production";
-const CALLBACK_URL = isProd 
-  ? "https://blushbook-api.onrender.com/api/auth/google/callback" 
-  : "http://localhost:5000/api/auth/google/callback";
-
+const CALLBACK_URL =
+  process.env.GOOGLE_CALLBACK_URL ||
+  "http://localhost:5000/api/auth/google/callback";
+console.log("GOOGLE CALLBACK URL:", CALLBACK_URL);
 passport.use(
   new GoogleStrategy(
     {
